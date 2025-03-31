f1b42198a44d57cd4d1af2a1e25695d08c83bd0a
S GOMEZ RUIZ <165152002+SGR222@users.noreply.github.com>
2025-03-31 11:00:01 +0200

    Add files via upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+<h1>Hello World!</h1>
+<div class="logo">
+  <img src="../assets/logo1.jpg" class="logo">
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: rgba(41,52,57,100);
+  font-size: 2em;
+  font-weight: 400;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  text-align: center;
+}
+
+.logo{ 
+  width: 100px;
+  height: 100px;
+  margin: 0 auto;
+  display: block;
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+</style>
